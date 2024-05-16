1aeead473c9afd89f9aee9308b41e271eb7f1d28
eduardinhooo <129527327+eduardinhooo@users.noreply.github.com>
2024-05-15 22:39:19 -0300

    FETO

--- script.js
+++ script.js
@@ -5,8 +5,15 @@ document.querySelector("#yes").addEventListener("click", function () {
   let question = document.getElementById("question");
   let message = document.getElementById("answer");
 
-  // Alterando o fundo do cartão para um vídeo
-  card.innerHTML = '<video id="myVideo" width="100%" height="100%" autoplay loop muted><source src="https://s17.aconvert.com/convert/p3r68-cdx67/fj4ah-rwyt9.mp4" type="video/mp4"></video>';
+  // Alterando o fundo do cartão para um vídeo com áudio
+  card.innerHTML = `
+    <video id="myVideo" width="100%" height="100%" autoplay loop muted>
+      <source src="https://s17.aconvert.com/convert/p3r68-cdx67/fj4ah-rwyt9.mp4" type="video/mp4">
+    </video>
+    <audio autoplay loop>
+      <source src="seu_audio.mp3" type="audio/mp3">
+    </audio>
+  `;
 
   question.style.display = "none";
   message.style.display = "flex";
@@ -21,3 +28,4 @@ document.querySelector("#no").addEventListener("mouseover", function () {
   this.style.top = Math.random() * width + "px";
   this.style.left = Math.random() * height + "px";
 });
+
